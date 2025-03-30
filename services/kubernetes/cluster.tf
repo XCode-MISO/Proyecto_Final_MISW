111f94ee6c7b35c9f4933501ae8c635c4d121251
Andres Felipe Losada <nedrocoli@gmail.com>
2025-03-29 22:10:02 -0500

    finish kubernetes deployment

---
+++ services/kubernetes/cluster.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  project = "misw-4301-native-cloud-433702"
+}
+
+resource "google_container_cluster" "microservices-proyecto" {
+  name               = "microservices-proyecto"
+  location           = "us-central1"
+  enable_autopilot = true
+}
+
+
