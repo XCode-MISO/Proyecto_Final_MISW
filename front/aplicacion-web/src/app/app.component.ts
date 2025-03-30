9b5e034753f959930f70f2aec0dcc6577e912d2b
Andres Felipe Losada <nedrocoli@gmail.com>
2025-03-30 15:30:51 -0500

    add firebase deployment

--- front/aplicacion-web/src/app/app.component.ts
+++ front/aplicacion-web/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RoutesComponent } from "./routes/routes.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    RoutesComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/aplicacion-web/src/app/app.routes.ts
+++ front/aplicacion-web/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RoutesComponent } from './routes/routes.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: AppComponent },
+  { path: 'routes', component: RoutesComponent }
+];
