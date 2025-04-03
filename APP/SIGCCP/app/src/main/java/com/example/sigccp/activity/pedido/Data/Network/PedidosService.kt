05511903f7ba4f1f0421776a80ea5a20411f2bf8
Cristian Arnulfo Arias Vargas <ca.ariasv1@uniandes.edu.co>
2025-04-02 21:55:47 -0500

    Fix Pedido Service/ Refactor Utils.

--- APP/SIGCCP/app/src/main/java/com/example/sigccp/activity/pedido/Data/Network/PedidosService.kt
+++ APP/SIGCCP/app/src/main/java/com/example/sigccp/activity/pedido/Data/Network/PedidosService.kt
@@ -6,8 +6,7 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class PedidosService @Inject constructor(
-    private val pedidoListClient: PedidoListClient,
-    private val pedidoClient: PedidoClient,
+    private val pedidoListClient: ApiServicePedido,
 )
 {
     fun getPedidosFlow():Flow<List<DataItemPedido>> = flow {
@@ -23,7 +22,7 @@ class PedidosService @Inject constructor(
 
     fun getPedidoFlow(pedidoId:String):Flow<DataItemPedido> = flow{
         try {
-            val response = pedidoClient.getPedido(pedidoId)
+            val response = pedidoListClient.getPedidoId(pedidoId)
             emit(response)
         }
         catch (e: Exception)
