2c3e46fccd51a126f740cb8ac63aca19aee0aceb
Cristian Arnulfo Arias Vargas <ca.ariasv1@uniandes.edu.co>
2025-04-06 06:56:46 -0500

    libraris

--- APP/SIGCCP/app/src/main/java/com/example/sigccp/activity/pedido/UI/View/ListarPedido.kt
+++ APP/SIGCCP/app/src/main/java/com/example/sigccp/activity/pedido/UI/View/ListarPedido.kt
@@ -1,6 +1,5 @@
 package com.example.sigccp.activity.pedido.UI.View
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,35 +7,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.sigccp.R
 import com.example.sigccp.navigation.AppScreen
-import com.example.sigccp.ui.View.Components.ClientDropdown
 import com.example.sigccp.ui.View.Components.ListaDePedidos
-import com.example.sigccp.ui.View.Components.PedidoBox
 import com.example.sigccp.ui.View.Components.ScreenContainer
 import com.example.sigccp.ui.View.Components.locationDropdown
 import com.example.sigccp.ui.View.Components.newButton
-import com.example.sigccp.ui.View.Components.newMenuButton
 import com.example.sigccp.ui.View.clientes
 import com.example.sigccp.ui.View.listaPedidos
-import com.example.sigccp.ui.View.pedidoEjemplo
-import com.example.sigccp.ui.theme.AmarilloApp
-import com.example.sigccp.ui.theme.AppTypography
-import com.example.sigccp.ui.theme.VerdeApp
 
 //@Preview
 @Composable
