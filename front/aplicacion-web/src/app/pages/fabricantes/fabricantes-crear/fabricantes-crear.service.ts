8878cca0df44fc1a4531241fdc2ebb82602fdf29
Andres Felipe Losada <nedrocoli@gmail.com>
2025-04-07 15:24:07 -0500

    fix url

--- front/aplicacion-web/src/app/pages/fabricantes/fabricantes-crear/fabricantes-crear.service.ts
+++ front/aplicacion-web/src/app/pages/fabricantes/fabricantes-crear/fabricantes-crear.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../../../environments/environment';
   providedIn: 'root'
 })
 export class FabricantesCrearService {
-  private readonly apiUrl = `https://microservicios-gateway-1qkjvfz9.uc.gateway.dev/api/compras/fabricantes`;
+  private readonly apiUrl = `https://microservicios-gateway-1qkjvfz9.uc.gateway.dev/api/fabricantes`;
   constructor(private http: HttpClient) {}
 
   crearFabricante(fabricante: Fabricante): Observable<Fabricante> {
