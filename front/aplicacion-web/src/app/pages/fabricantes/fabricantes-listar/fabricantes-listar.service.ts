3b8d8cb6db9ef3ac972f253ecb54925e0f85302e
EsneiderRE <e.restrepoe@uniandes.edu.co>
2025-04-06 20:14:13 -0500

    Se cambia variable de entorno de produccion para que apunte al api gateway

--- front/aplicacion-web/src/app/pages/fabricantes/fabricantes-listar/fabricantes-listar.service.ts
+++ front/aplicacion-web/src/app/pages/fabricantes/fabricantes-listar/fabricantes-listar.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../../../environments/environment';
 
 export type Fabricante = {
   id: number;
@@ -14,8 +14,8 @@ export type Fabricante = {
   providedIn: 'root'
 })
 export class FabricantesListarService {
-  private apiUrl = 'https://67f142f7c733555e24aca5a3.mockapi.io/misw-pruebas/fabricantes';
-  //private apiUrl = 'http://compras.cppxcode.shop/api/fabricantes'; 
+  //private apiUrl = 'https://67f142f7c733555e24aca5a3.mockapi.io/misw-pruebas/fabricantes';
+  private readonly apiUrl = `${environment.apiUrl}/api/fabricantes`;
   constructor(private http: HttpClient) {}
 
   obtenerFabricantes(): Observable<Fabricante[]> {
--- front/aplicacion-web/src/app/pages/productos/productos-cargar/productos.service.ts
+++ front/aplicacion-web/src/app/pages/productos/productos-cargar/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 export interface FabricanteDto {
   id: number;
@@ -16,7 +17,7 @@ export interface ProductoDto {
 
 @Injectable({ providedIn: 'root' })
 export class ProductosService {
-  private readonly apiUrl = '/api';
+  private readonly apiUrl = `${environment.apiUrl}/api`;
 
   constructor(private http: HttpClient) {}
 
