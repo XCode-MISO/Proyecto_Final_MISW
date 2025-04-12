1b12176a8e2bde5f59e0b7edd9062c92a3c76233
Cristian Arnulfo Arias Vargas <ca.ariasv1@uniandes.edu.co>
2025-04-12 18:29:53 -0500

    Fix Pequeños

--- services/pedidos/src/commands/get_clientes.py
+++ services/pedidos/src/commands/get_clientes.py
@@ -1,22 +1,14 @@
 from .base_command import BaseCommannd
-from ..models.cliente import Cliente, ClienteJsonSchema
+from src.models.pedido import ClientSchema
 from src.database import db
 
 class GetClientes(BaseCommannd):
     def execute(self):
         print("DEBUG: Entrando en GetClientes.execute()")
-
-        session = db.session()
         try:
-            clientes = session.query(Cliente).all()
-            print(f"DEBUG: Se encontraron {len(clientes)} clientes")
+            print(f"DEBUG: Se encontraron {len()} clientes")
 
-            # Serializar los clientes
-            clientes_json = ClienteJsonSchema(many=True).dump(clientes)
         except Exception as e:
             print(f"ERROR: {str(e)}")
             return {"error": "Error al obtener los clientes"}, 500
-        finally:
-            session.close()
-
-        return clientes_json
+        return {"clientes"}, 200
