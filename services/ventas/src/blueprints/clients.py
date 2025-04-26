7b21b306a1a2cb6661aa6634f3afd16d76482ab3
Andres Felipe Losada <nedrocoli@gmail.com>
2025-04-26 11:52:55 -0500

    fix clients

--- services/ventas/src/blueprints/clients.py
+++ services/ventas/src/blueprints/clients.py
@@ -7,17 +7,17 @@ from ..commands.get__all_clients import GetAllClients
 
 client_blueprint = Blueprint('clients', __name__)
 
-@client_blueprint.route('/clients', methods=['POST'])
+@client_blueprint.route('/api/clients', methods=['POST'])
 def create():
     client = CreateClient(request.get_json()).execute()
     return jsonify(client), 201
 
-@client_blueprint.route('/clients/<id>', methods=['GET'])
+@client_blueprint.route('/api/clients/<id>', methods=['GET'])
 def getClient(id):
     client = GetClient(id).execute()
     return jsonify(client)
 
-@client_blueprint.route('/clients', methods=['GET'])
+@client_blueprint.route('/api/clients', methods=['GET'])
 def getAllClients():
     clients = GetAllClients().execute()
     return jsonify(clients)
