7e9bfd297020cb55aca57d9febc71878c59d609d
echaparroa-uniandes <e.chaparroa@uniandes.edu.co>
2025-05-09 09:26:54 -0500

    test e2e registrar visita

---
+++ APP/SIGCCP/app/src/androidTest/java/com/example/sigccp/RegistrarVisitaE2eTest.kt
@@ -0,0 +1,28 @@
+package com.example.sigccp
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RegistrarVisitaE2eTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+    private val robot = MainActivityRobot(composeTestRule)
+
+    @Test
+    fun registrarVisitaSeMuestraTest() {
+        robot.escribirEnCampo("Correo", "vendedor@gmail.com")
+        robot.escribirEnCampo("Contraseña", "12345678")
+        robot.clickEnBoton("Ingresar")
+        robot.esperarProcesamiento(3000)
+        robot.verificarScreenConTituloSeMuestra("¡Hello, Seller!")
+        robot.clickEnBoton("REGISTER VISIT")
+        robot.verificarScreenConTituloSeMuestra("Visit Register")
+        robot.clickEnBoton("logout")
+        robot.esperarProcesamiento(500)
+    }
+}
+
