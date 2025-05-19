ebb61b68b45727a4fbd637deecd80792c15559e2
EsneiderRE <e.restrepoe@uniandes.edu.co>
2025-05-18 22:37:03 -0500

    fix: correcion vista reporte

--- front/aplicacion-web/src/app/pages/ventas/vendedor/vendedor-reportes/vendedor-reportes.component.spec.ts
+++ front/aplicacion-web/src/app/pages/ventas/vendedor/vendedor-reportes/vendedor-reportes.component.spec.ts
@@ -1,15 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { MatTableModule } from '@angular/material/table';
+import { TranslateModule, TranslateStore } from '@ngx-translate/core';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { VendedorReportesComponent } from './vendedor-reportes.component';
 
-fdescribe('VendedorReportesComponent', () => {
+describe('VendedorReportesComponent', () => {
   let component: VendedorReportesComponent;
   let fixture: ComponentFixture<VendedorReportesComponent>;
 
@@ -18,11 +17,12 @@ fdescribe('VendedorReportesComponent', () => {
       imports: [
         VendedorReportesComponent,
         HttpClientTestingModule,
-        TranslateModule.forRoot(),
-        MatTableModule
+        RouterTestingModule,
+        TranslateModule.forRoot()
       ],
       providers: [
         provideNativeDateAdapter(),
+        TranslateStore,
         {
           provide: ActivatedRoute,
           useValue: {
@@ -35,9 +35,10 @@ fdescribe('VendedorReportesComponent', () => {
         }
       ],
       schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(VendedorReportesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
